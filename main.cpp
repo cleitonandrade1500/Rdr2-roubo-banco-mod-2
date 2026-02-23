127eb7686cc653dc5a0c299db65e5955120e4754
cleitonandrade1500 <cleitonandrade1500@gmail.com>
2026-02-23 20:51:58 -0300

    Excluir main.cpp

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "script.h"
-
-extern "C" __attribute__((visibility("default"))) int module_start(size_t argc, const void* argv) {
-    scriptMain();
-    return 0;
-}
-extern "C" int module_stop(size_t argc, const void* argv) { return 0; }
